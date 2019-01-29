5f2711e5c41b8fc448a725e0f72be43df4caad2b
Massimo Ganzerli <massimoganzerli@yahoo.com>
2019-01-29 14:52:51 +0100

    result set display, and single item page

--- server/routes/index.js
+++ server/routes/index.js
@@ -20,7 +20,8 @@ module.exports = function() {
   // get the store
   router.get("/store", (req, res) => {
     Items.findAll().then(items => {
-      console.log(items);
+      //console.log(items[1].dataValues);
+      // passing the variables from backend to the view
       res.render("store", {
         pageTitle: "store",
         items
@@ -28,17 +29,34 @@ module.exports = function() {
     });
   });
 
+  // Details single item
+  router.get("/itemDetails/:id", (req, res) => {
+    // FITN ONE limiting the find all to 1
+    Items.findAll({
+      limit: 1,
+      where: {
+        id: req.params.id
+      },
+      order: [["createdAt", "DESC"]]
+    }).then(allItems => {
+      res.render("itemDetails", {
+        item: allItems[0],
+        pageTitle: "Details"
+      });
+    });
+  });
+
   // add records manually form here hardcoded
   router.get("/add", (req, res) => {
     Items.create({
-      title: "Item2",
-      body: "very nice item here ",
-      special: "extremely",
+      title: "X box",
+      body: "I have an X box collection version to give away..",
+      special: "x box collection",
       updatedAt: new Date().toLocaleString(),
       createdAt: new Date().toLocaleString(),
-      selected: true,
-      prize: 1000,
-      img: "veryImage2.png"
+      selected: false,
+      prize: 300,
+      img: "veryImage7.png"
     }).then(data => {
       console.log(data);
     });
