70525f99a2a388e102c09cec102d966e9172909b
Massimo Ganzerli <massimoganzerli@yahoo.com>
2019-02-23 02:24:27 +0100

    setup js lens

--- public/js/imgScan.js
+++ public/js/imgScan.js
@@ -1,1 +1,31 @@
-console.log("something from the imgScan");
+const img = document.querySelector("[js-attr=img-detail]");
+const lens = document.querySelector("[js-attr=img-lens]");
+
+console.log(lens);
+
+if (img && lens) {
+  console.log(img);
+
+  const coordinates = {
+    left: img.offsetLeft,
+    top: img.offsetTop,
+    right: img.offsetWidth + img.offsetLeft,
+    bottom: img.offsetHeight + img.offsetTop
+  };
+
+  console.log(coordinates);
+  img.addEventListener("mousemove", e => {
+    //
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+    const mapX = e.clientX - coordinates.left;
+    const mapY = e.clientY - coordinates.top;
+    //
+    lens.style.backgroundImage = `url(${e.target.getAttribute("src")})`;
+
+    //
+    console.log(mapX, mapY);
+    //
+  });
+}
